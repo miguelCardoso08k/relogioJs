f42933800a404d78d1d7b12d231ebb2e569637d6
Miguel Cardoso <joaomiguel.cardoso2006@outlook.com>
2023-03-28 19:40:43 -0300

    finalização do projeto

---
+++ assets/script.js
@@ -0,0 +1,42 @@
+const msg = window.document.getElementById("msg");
+const foto = window.document.getElementById("foto");
+let data;
+let horas;
+let $minutos;
+
+const relogio = setInterval(function () {
+  time();
+  imagens();
+  background();
+});
+
+function time() {
+  data = new Date();
+  horas = data.getHours();
+  $minutos = data.getMinutes();
+
+  if (horas < 10) horas = "0" + horas;
+  if ($minutos < 10) $minutos = "0" + $minutos;
+
+  msg.innerHTML = `<p>Agora são ${horas}:${$minutos}<p>`;
+}
+
+function imagens() {
+  if (horas > 0 && horas < 12) {
+    foto.src = "assets/imagens/manha.jpg";
+  } else if (horas > 12 && horas < 18) {
+    foto.src = "assets/imagens/tarde.jpg";
+  } else {
+    foto.src = "assets/imagens/noite.jpg";
+  }
+}
+
+function background() {
+  if (horas > 0 && horas < 12) {
+    document.body.style.background = "#E5CC83";
+  } else if (horas > 12 && horas < 18) {
+    document.body.style.background = "#F59B53";
+  } else {
+    document.body.style.background = "#0A2761";
+  }
+}
